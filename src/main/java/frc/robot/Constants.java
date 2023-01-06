1cc129c29aefc46c0532de64e1159d5845c4b3dd
kytpbs <kytpbs@gmail.com>
2023-01-06 08:07:26 +0300

    Addded arcade drive and did bug fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,8 +10,8 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 public final class Constants{
     public static PWMSparkMax leftMotor1 = new PWMSparkMax(0);
     public static PWMSparkMax leftMotor2 = new PWMSparkMax(1);
-    public static PWMSparkMax rightMotor1 = new PWMSparkMax(3);
-    public static PWMSparkMax rightMotor2 = new PWMSparkMax(4);
+    public static PWMSparkMax rightMotor1 = new PWMSparkMax(2);
+    public static PWMSparkMax rightMotor2 = new PWMSparkMax(3);
     public static MotorControllerGroup rightMotorsGroup = new MotorControllerGroup(rightMotor1,rightMotor2);
     public static MotorControllerGroup leftMotorsGroup  = new MotorControllerGroup(leftMotor1, leftMotor2);
     public static DifferentialDrive driveTrain = new DifferentialDrive(leftMotorsGroup,rightMotorsGroup);
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -94,7 +94,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    driveTrain.arcadeDrive(stick.getY(), -stick.getZ());
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
